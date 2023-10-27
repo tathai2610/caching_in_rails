b68bb6b66d26492f94fa2fa8a08d550641c46073
tathai <tathai2610@gmail.com>
2023-10-27 09:43:30 +0700

    scaffold company and employee

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+30.times do
+  company = Company.new name: Faker::Company.name
+  if company.save
+    SecureRandom.random_number(100).times do
+      company.employees.create first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, phone_number: Faker::PhoneNumber.phone_number
+    end
+  end
+end
